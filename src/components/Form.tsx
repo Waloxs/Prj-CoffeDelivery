a12fe053971c9f3d707f585db74ba66744827017
Waloxs <waloxgustavo0@gmail.com>
2025-05-04 21:08:59 -0300

    responsivo

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -56,7 +56,10 @@ const Form = () => {
     }
 
     return (
-        <div className="grid mt-[4rem] mb-[3rem] pl-[140px] pr-[140px] grid-cols-[2fr_1.5fr] gap-[3rem]">
+        <div className="grid mt-[4rem] mb-[3rem] pl-[140px] pr-[140px] grid-cols-[2fr_1.5fr] gap-[3rem] 
+        max-[1200px]:grid-cols-[1fr]
+        max-[800px]:px-[40px]
+        max-[500px]:px-[10px]">
           <div className="flex flex-col gap-4">
             <span className={`${baloo.className} font-medium`}>Complete seu pedido</span>
 
@@ -130,35 +133,46 @@ const Form = () => {
                         <img src={dollar.src} alt="" className="object-contain self-start"/> 
                         <div className="flex flex-col">
                             <span>Pagamento</span>
-                            <span className="text-[#574F4D] text-sm font-medium">O pagamento é feito na entrega. Escolha a forma que deseja pagar</span>
+                            <span className="text-[#574F4D] text-sm font-medium 
+                            max-[600px]:text-[12px]
+                            max-[540px]:text-[10px]">O pagamento é feito na entrega. Escolha a forma que deseja pagar</span>
                         </div>
                     </div>
 
 
                     <div className="grid grid-cols-3 gap-5">
                         <div 
-                            className={`flex items-center gap-2 bg-[#E6E5E5] p-4 border-2 ${selected === 'debito' ? 'border-[#8047F8]' : 'border-transparent'} cursor-pointer`}
+                            className={`flex items-center justify-center gap-2 bg-[#E6E5E5] p-4 border-2 ${selected === 'debito' ? 'border-[#8047F8]' : 'border-transparent'} cursor-pointer`}
                             onClick={() => handleSelected('debito')}>
 
                             <img src={debito.src} alt="" className="w-[15px]"/>
-                            <span className="text-[#574F4D] text-xs">CARTÃO DE DÉBITO</span>
+                            <span className="text-[#574F4D] text-xs max-[895px]:text-[10px] max-[515px]:text-[8px]">
+                                <span className="max-[640px]:hidden">CARTÃO DE CRÉDITO</span>
+                                <span className="hidden max-[640px]:inline">CRÉDITO</span>
+                            </span>
+
                         </div>
 
                         <div 
-                          className={`flex items-center gap-2 bg-[#E6E5E5] p-4 border-2 ${selected === 'credito' ? 'border-[#8047F8]' : 'border-transparent'} cursor-pointer`}
+                          className={`flex items-center justify-center gap-2 bg-[#E6E5E5] p-4 border-2 ${selected === 'credito' ? 'border-[#8047F8]' : 'border-transparent'} cursor-pointer`}
                           onClick={() => handleSelected('credito')}
                           >
                           
                             <img src={credito.src} alt="" className="w-[15px]"/>
-                            <span className="text-[#574F4D] text-xs">CARTÃO DE CRÉDITO</span>
+                            <span className="text-[#574F4D] text-xs max-[895px]:text-[10px]
+                            max-[515px]:text-[8px]">
+                                <span className="max-[640px]:hidden">CARTÃO DE DÉBITO</span>
+                                <span className="hidden max-[640px]:inline">DÉBITO</span>
+                            </span>
                         </div>
 
                         <div 
-                            className={`flex items-center gap-2 bg-[#E6E5E5] p-4 border-2 ${selected === 'dinheiro' ? 'border-[#8047F8]' : 'border-transparent'} cursor-pointer`}
+                            className={`flex items-center justify-center gap-2 bg-[#E6E5E5] p-4 border-2 ${selected === 'dinheiro' ? 'border-[#8047F8]' : 'border-transparent'} cursor-pointer`}
                             onClick={() =>handleSelected('dinheiro')}>
 
                             <img src={dinheiro.src} alt="" className="w-[15px]"/>
-                            <span className="text-[#574F4D] text-xs">DINHEIRO</span>
+                            <span className="text-[#574F4D] text-xs max-[895px]:text-[10px]
+                            max-[515px]:text-[8px]">DINHEIRO</span>
                         </div>
                     </div>
 
