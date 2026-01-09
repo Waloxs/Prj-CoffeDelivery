5918b1bfc8228f7b9aca4d087b6ead9309ff67ce
Waloxs <waloxgustavo0@gmail.com>
2026-01-08 23:30:07 -0300

    Atualizando backen

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,29 +1,25 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import routes from '../api/routes/users'
+import routes from '../api/routes/users';
 
 dotenv.config();
 
-// src/api.js
 const BASE_URL = "https://prj-coffedelivery.onrender.com/api";
-
 export default BASE_URL;
-
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.get('/api', (req, res) => {
   res.send('Bem-vindo à API');
 });
 
-
 app.use('/api', routes);
 
-
-
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
